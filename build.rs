270a5e028de3731c68f9ac71e8276d928b2b8b59
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-09-11 20:52:22 +0900

    build: init

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("ime-alt.ico");
+        res.set("FileDescription", "Left/right Alt for IME on/off.");
+        res.set("FileVersion", "0.1.0.0");
+        res.set("LegalCopyright", "Copyright (C) mammb.com. All rights reserved.");
+        res.set("ProductName", "ime-alt");
+        res.set("OriginalFilename", "ime-alt.exe");
+        res.set("ProductVersion", "0.1.0.0");
+        res.compile().unwrap();
+    }
+}
